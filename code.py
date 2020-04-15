16d1dd47b4af762caa7dc48150b1210799c60e92
Kyle Gearhart <hkgearhart@gmail.com>
2020-04-15 08:25:20 +0200

    Add new-line at the end of the 5-rows print

--- code.py
+++ code.py
@@ -44,6 +44,7 @@ def print_first_five_rows_of_data(data_rows):
     print('First five rows in dataset:')
     for row_index in range(5):
         print(data_rows[row_index])
+    print('')
 
 
 def load_dataset_csv_file(file_path):
