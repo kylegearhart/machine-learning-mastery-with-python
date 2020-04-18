8539d74eb32c5b9dd477cb94f8d706797261c300
Kyle Gearhart <hkgearhart@gmail.com>
2020-04-18 10:20:45 +0200

    Add distinguishing information into the output for zero-rule model for classification

--- code.py
+++ code.py
@@ -227,7 +227,8 @@ def zero_rule_algorithm_for_classification(training_dataset, test_dataset):
     all_classes_in_training_dataset = [row[last_column_index] for row in training_dataset]
     most_common_class = max(set(all_classes_in_training_dataset), key=all_classes_in_training_dataset.count)
     predictions_on_test_dataset = [most_common_class for _ in range(len(test_dataset))]
-    print('Zero-Rule predictions on test dataset: {0}\n'.format(predictions_on_test_dataset))
+    print('Zero-Rule predictions on test dataset (classification/most-common class):\n{0}\n'
+          .format(predictions_on_test_dataset))
     return predictions_on_test_dataset
 
 
