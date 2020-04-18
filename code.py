87fed3793f762a17cd681721e430957635ad8480
Kyle Gearhart <hkgearhart@gmail.com>
2020-04-18 09:58:25 +0200

    Add random-prediction baseline model

--- code.py
+++ code.py
@@ -210,6 +210,18 @@ def calculate_root_mean_squared_error(actual_values, predicted_values):
     return root_mean_squared_error
 
 
+def random_prediction_algorithm(training_dataset, test_dataset):
+    last_column_index = -1
+    all_predictions_in_training_dataset = [row[last_column_index] for row in training_dataset]
+    set_of_possible_predictions = list(set(all_predictions_in_training_dataset))
+    random_predictions_on_test_dataset = list()
+    for _ in range(len(test_dataset)):
+        random_prediction_index = randrange(len(set_of_possible_predictions))
+        random_predictions_on_test_dataset.append(set_of_possible_predictions[random_prediction_index])
+    print('Random predictions on test dataset: {0}\n'.format(random_predictions_on_test_dataset))
+    return random_predictions_on_test_dataset
+
+
 seed(1)  # Ensure that results are always the same
 
 normalized_pima_dataset = preprocess_and_normalize_pima_indians_diabetes_dataset()
