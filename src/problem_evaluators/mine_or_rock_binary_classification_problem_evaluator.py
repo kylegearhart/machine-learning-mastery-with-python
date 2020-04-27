ee3d1e64df25a8e5a99c1baec467f34027bae265
Kyle Gearhart <hkgearhart@gmail.com>
2020-04-27 08:23:21 +0200

    Add baseline evaluation for mine-or-rock-sonar binary classification problem

---
+++ src/problem_evaluators/mine_or_rock_binary_classification_problem_evaluator.py
@@ -0,0 +1,23 @@
+from src.algorithm_evaluation import evaluate_classification_algorithm_with_k_fold_cross_validation
+from src.algorithms.classification_algorithms import zero_rule_algorithm_for_classification
+from src.data_preprocessing import load_dataset_csv_file, convert_data_to_floats_in_column_range, \
+    convert_string_class_names_to_ints_for_column
+
+
+def evaluate_candidate_algorithms_for_mine_or_rock_sonar_binary_classification_problem(num_cross_validation_folds):
+    mine_or_rock_sonar_dataset = preprocess_mine_or_rock_sonar_dataset()
+
+    evaluate_classification_algorithm_with_k_fold_cross_validation(
+        mine_or_rock_sonar_dataset,
+        zero_rule_algorithm_for_classification,
+        num_cross_validation_folds
+    )
+
+
+def preprocess_mine_or_rock_sonar_dataset():
+    dataset = load_dataset_csv_file('datasets/mine-or-rock-sonar.data.csv')
+    convert_data_to_floats_in_column_range(dataset,
+                                           range(0, len(dataset[0]) - 1))
+    convert_string_class_names_to_ints_for_column(dataset, -1)
+
+    return dataset
